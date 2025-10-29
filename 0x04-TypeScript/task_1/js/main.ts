fafccbd083dfa174753f0fb082fb910d724caa7f
Benict <152749576+Benict@users.noreply.github.com>
2025-10-29 14:42:30 +0000

    Building a teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher  {
+  readonly firstName :string 
+  readonly lastName : string
+  fullTimeEmployee : boolean
+  location : string
+  yearsOfExperience? : number
+  [key : string] : any
+}
+
+let firstTeacher : Teacher = {
+  firstName : 'Lloyd',
+  lastName : 'Asiedu',
+  fullTimeEmployee : true,
+  location : 'Tema',
+  yearsOfExperience : 4,
+  Loyal : true
+}
+
