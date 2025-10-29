18dba61dbd7f5142e0247728d95cffc474d113d3
Benict <152749576+Benict@users.noreply.github.com>
2025-10-29 15:39:06 +0000

    final work

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,18 +1,17 @@
 interface Teacher  {
-  readonly firstName :string 
-  readonly lastName : string
-  fullTimeEmployee : boolean
-  location : string
-  yearsOfExperience? : number
-  [key : string] : any
+  readonly firstName: string
+  readonly lastName: string
+  fullTimeEmployee: boolean
+  location: string
+  yearsOfExperience?: number
+  [key: string]: any
 }
 
 let firstTeacher : Teacher = {
-  firstName : 'Lloyd',
-  lastName : 'Asiedu',
-  fullTimeEmployee : true,
-  location : 'Tema',
-  yearsOfExperience : 4,
-  Loyal : true
-}
-
+  firstName: 'Lloyd',
+  lastName: 'Asiedu',
+  fullTimeEmployee: true,
+  location: 'Tema',
+  yearsOfExperience: 4,
+  Loyal: true
+};
