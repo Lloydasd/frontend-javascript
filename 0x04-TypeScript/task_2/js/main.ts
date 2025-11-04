142b5ea2e2f4b2946c05a0fa191e29824d4f8eab
Benict <152749576+Benict@users.noreply.github.com>
2025-11-04 13:58:35 +0000

    Solved all the issues

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,63 @@
+interface DirectorInterface {
+  workFromHome():string
+  getCoffeeBreak():string
+  workDirectorTasks(): string
+}
+
+interface TeacherInterface {
+  workFromHome():string
+  getCoffeeBreak():string
+  workTeacherTasks(): string
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home'
+  }
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break'
+  }
+  workDirectorTasks(): string {
+    return 'Getting to director tasks'
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home'
+  }
+  getCoffeeBreak(): string {
+    return 'Cannot have a break'
+  }
+  workTeacherTasks(): string {
+    return 'Getting to work'
+  }
+}
+
+function createEmployee(salary: number|string){
+   if( typeof salary ==='number' && salary < 500){
+    return new Teacher()
+  }else {
+    return new Director()
+  }
+}
+
+console.log(createEmployee(200));
+
+function isDirector(employee: Teacher | Director):employee is Director{
+  return employee instanceof Director
+}
+
+function executeWork(employee : Teacher | Director){
+  if(isDirector(employee)){
+    return employee.workDirectorTasks()
+  }else {
+    return employee.workTeacherTasks()
+  }
+}
+
+const director = new Director();
+const teacher = new Teacher ();
+
+console.log(executeWork(director));
+console.log(executeWork(teacher));
